7f727cf1175478b27046175f36c8bd9feb3141e6
meylichiaafifaharmi05 <aafifah.meylichia@gmail.com>
2018-10-05 20:31:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/meylichia/myapplication/UIlogin.java
@@ -0,0 +1,49 @@
+package com.example.meylichia.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button btnLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        username = (EditText) findViewById(R.id.User name);
+        password = (EditText) findViewById(R.id.Password);
+        btnLogin = (Button)findViewById(R.id.Sign in);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String usernameKey = username.getText().toString();
+                String passwordKey = password.getText().toString();
+
+                if (usernameKey.equals("admin") && passwordKey.equals("123")){
+                    //jika login berhasil
+                    Toast.makeText(getApplicationContext(), "LOGIN SUKSES",
+                            Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, Welcome.class);
+                    MainActivity.this.startActivity(intent);
+                    finish();
+                }else {
+                    //jika login gagal
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Username atau Password Anda salah!")
+                            .setNegativeButton("Retry", null).create().show();
+                }
+            }
+
+        });
+
+    }
+}
